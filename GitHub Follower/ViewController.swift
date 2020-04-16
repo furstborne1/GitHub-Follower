7206b2d4cdd3549e3fca1cf785f5eb53be911fff
furstborne1 <34493768+furstborne1@users.noreply.github.com>
2020-04-16 18:18:28 -0400

    Initial Commit

---
+++ GitHub Follower/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitHub Follower
+//
+//  Created by MARC on 4/16/20.
+//  Copyright © 2020 MARC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
